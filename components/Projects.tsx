d9355935049fce02afed256e9f84b7df7d29cd75
Kashish Jain <9682284-Kashishjain04@users.noreply.gitlab.com>
2022-09-19 10:42:28 +0530

    last project-mockup slide

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,7 +20,7 @@ const Projects = ({ projects }: Props) => {
 						className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
 					>
 						<motion.img
-							initial={{ y: -300, opacity: 0 }}
+							initial={{ y: -200, opacity: 0 }}
 							whileInView={{ y: 0, opacity: 1 }}
 							transition={{ duration: 1.2 }}
 							viewport={{ once: true }}
